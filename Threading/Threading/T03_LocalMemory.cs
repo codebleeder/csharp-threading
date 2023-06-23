1d22cd5d8e510a46af6437c6bad37ebeb8f9c6f6
sharad.shivmath <sharad.shivmath@zapcg.com>
2023-06-23 16:29:56 +0530

    demo 3: local memory

---
+++ Threading/Threading/T03_LocalMemory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    public static class T03_LocalMemory
+    {
+        public static void Run()
+        {
+            // worker thread
+            var thread = new Thread(PrintOneToThirty);
+            thread.Start();
+
+            // main thread
+            PrintOneToThirty();
+        }
+
+        public static void PrintOneToThirty()
+        {
+            /* each thread - worker and main thread 
+             * will have its own
+             * copy of variable i stored in its 
+             * stack
+             */
+            for(var i=1; i<31; ++i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
